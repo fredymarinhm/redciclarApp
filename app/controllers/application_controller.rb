0d8cbc2be200fdd0ec2167485a598bd192d81546
Fredy <fredymarinduarte@hotmail.com>
2018-08-08 17:16:21 -0500

    Cambio dinámico de entorno

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_devise_params, if: :devise_controller?
+  $URLSERVER = ''
+  before_action :thatEnvirnoment
+  
 
   def configure_devise_params
     devise_parameter_sanitizer.permit(:sign_up) do |user|
       user.permit(:name, :email, :password, :password_confirmation)
-      #Borrar comentario
     end
   end
 
+  def thatEnvirnoment
+    if Rails.env.procution?
+      $URLSERVER = 'https://redciclar.herokuapp.com/';
+    else
+      $URLSERVER = 'http://localhost:3000/';
+    end
+  end
 end
