c9c0a66c3d00e3bb28438b9b48f8d8e5d93933b3
Fredy <fredymarinduarte@hotmail.com>
2018-08-10 18:07:36 -0500

    server

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,16 @@
-CarrierWave.configure do |config|
-    config.storage = :fog
-    config.fog_porvider = 'fog/amazon'
-    config.fog_credentials = {
-        provider:              'AWS',
-        aws_access_key_id:     'AKIAJE4XJSYIBHE3XIXA',
-        aws_secret_access_key: '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',
-        region:                'us-east-1',
-    }
-    config.fog_directory  = 'redciclar'
+require 'carrierwave/storage/fog'
+
+if Rails.env.production?
+	CarrierWave.configure do |config|
+		config.storage = :fog
+		config.fog_porvider = 'fog'
+		config.fog_credentials = {
+			provider:              'AWS',
+			aws_access_key_id:     'AKIAJE4XJSYIBHE3XIXA',
+			aws_secret_access_key: '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',
+			region:                'us-east-1',
+		}
+		config.fog_directory  = 'redciclar'
+		
+	end
 end
